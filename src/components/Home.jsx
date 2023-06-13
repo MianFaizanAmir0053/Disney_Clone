97f1dbc67eb79775fb10409b188463b1002a572b
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-06-14 04:38:45 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import api from "../API/api";
 import Slider from "./Slider";
 import Header from "./Header";
 import MovieSlider from "./MovieSlider";
-import ImageVideo from "./imageVideo";
+import ImageVideo from "./ImageVideo";
 const imgApi = "https://image.tmdb.org/t/p/w1280/";
 const api2 =
   "https://api.themoviedb.org/3/discover/tv?api_key=44ca6748d561037c21b26e7e1d17ec3e&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0";
