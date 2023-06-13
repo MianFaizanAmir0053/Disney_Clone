9c60169a2232cd2cd668b599e7e916eb87b8fc10
MianFaizanAmir0053 <97526215+MianFaizanAmir0053@users.noreply.github.com>
2023-06-14 03:54:46 +0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      // Add the "react-router-dom" module as an external dependency
+      // to prevent it from being bundled with your application
+      // and to resolve the import issue.
+      esbuild: {
+        external: ['react-router-dom'],
+      },
+    }),
+  ],
+});
